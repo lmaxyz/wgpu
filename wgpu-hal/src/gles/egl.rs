cec7fb20d0978141fef8bfc43f956e9ce4b3f79c
Maxim Leyman <maximleyman@yandex.ru>
2025-02-26 21:40:06 +0300

    debug egl

--- wgpu-hal/src/gles/egl.rs
+++ wgpu-hal/src/gles/egl.rs
@@ -853,7 +853,7 @@ impl crate::Instance for Instance {
                     }.unwrap()
                 } else {
                     log::info!("Using Wayland under android platform");
-                    unsafe {egl.get_display(library.display.as_ptr())}.unwrap()
+                    unsafe {egl.get_display(khronos_egl::DEFAULT_DISPLAY)}.unwrap()
                 };
                 (display, None, WindowKind::Wayland)
             } else if let (Some(display_owner), Some(egl)) = (x11_display_library, egl1_5) {
